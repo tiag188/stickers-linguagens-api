0c0c840a9151b02b42f246e9ac3fd7dac94d2f94
Tiago Silva <70734516+tiag188@users.noreply.github.com>
2023-04-03 23:14:40 -0300

    feat: criar post

--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -4,21 +4,23 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LinguagemController {
   // private List<Linguagem> languages = List.of(
-  //   new Linguagem(
-  //     "Java",
-  //     "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
-  //     1
-  //   ),
-  //   new Linguagem(
-  //     "PHP",
-  //     "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
-  //     2
-  //   )
+  // new Linguagem(
+  // "Java",
+  // "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
+  // 1
+  // ),
+  // new Linguagem(
+  // "PHP",
+  // "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
+  // 2
+  // )
   // );
 
   @Autowired
@@ -26,8 +28,14 @@ public class LinguagemController {
 
   @GetMapping("/languages")
   public List<Linguagem> getLanguages() {
-    //List<Linguagem> languages = repository.findAll();
-    //return languages;
+    // List<Linguagem> languages = repository.findAll();
+    // return languages;
     return repository.findAll();
+  }
+
+  @PostMapping("/languages")
+  public Linguagem postLanguage(@RequestBody Linguagem languageData){
+    Linguagem saveLanguage = repository.save(languageData);
+    return saveLanguage;
   }
 }
